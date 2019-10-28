cc49b82d3f4f239657eeab5c22e48fb3d2ecc013
Biljana <zabiljanupavlovic@gmail.com>
2019-10-28 22:29:53 +0100

    izmena domaceg

--- Nedelja2/dan1/zadatak1/zadatak1.js
+++ Nedelja2/dan1/zadatak1/zadatak1.js
@@ -1,12 +1,12 @@
 var a;
 var b;
 
-a=5;
-b=6;
+a=9;
+b=10;
 
 console.log(a+b);
 console.log(a-b);
 console.log(a*b);
 console.log(a/b);
 console.log(a%b);
-console.log(a^b);
+console.log(Math.pow(a,b));
