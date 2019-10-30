bbeb9b56f9e763989f88ff309fdbb693c5f0e2c0
Biljana <zabiljanupavlovic@gmail.com>
2019-10-30 10:11:01 +0100

    domaci

---
+++ Nedelja2/zadatak3.js
@@ -0,0 +1,8 @@
+
+var a=8;
+
+if(a%3==0){
+    console.log(`Broj je deljiv sa 3.`)
+}else{
+    console.log(`Broje nije deljiv sa 3`)
+};
---
+++ Nedelja2/zadatak4.js
@@ -0,0 +1,6 @@
+var a;
+for(a=1; a<5000; a++){
+    if (a%5==0){
+        console.log(a);
+    }
+}
