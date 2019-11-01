a711534b60f43fb001da8cd3e510a5cf96f52914
Biljana <zabiljanupavlovic@gmail.com>
2019-11-01 01:07:13 +0100

    domaci

--- Nedelja2/zadatak5.js
+++ Nedelja2/zadatak5.js
@@ -1,6 +1,8 @@
     var c = 100;
     var p = 5;
-    var cPc = c/(Math.sqrt(p/2)*Math.PI);
+    var r =p/2;
+   var pov =Math.pow(r,2)*Math.PI;
+   var cPc= c/pov;
     console.log(`Cena pice je ${cPc} dinara po cm²`);
 
 
